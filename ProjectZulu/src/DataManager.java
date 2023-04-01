a37d1c1e83a9406bdd22d8f3838c9f442b02b479
Luana Madeira <105006518+Luanarm@users.noreply.github.com>
2023-04-01 03:19:18 -0400

    Implemented call to the db to collect data

--- ProjectZulu/src/DataManager.java
+++ ProjectZulu/src/DataManager.java
@@ -3,44 +3,25 @@ import org.jfree.chart.*;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 public class DataManager {
-    public static void fillDataSet(DefaultCategoryDataset dataset, String location){
-        try {
-			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/database","root","root");
-
-			PreparedStatement statement = connection.prepareStatement("SELECT * from location");
-            statement.setString(1, location); 
-          
-            ResultSet AllData = statement.executeQuery();
-
-			while(AllData.next()) {
-                System.out.println("Looping");
-                dataset.addValue(AllData.getInt(4), "NHPI", AllData.getString(2));
-            }
-
-			//connection.close();
-		}
-
-		catch (Exception e) {
-			System.out.println(e);
-		}
-	}
-    
-    public static void main(String args[]){
-		try {
-			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/database","root","root");
-
-			Statement statement = connection.createStatement();
-			ResultSet resultset = statement.executeQuery("SELECT * from location");
-
-			while(resultset.next()) {
-				System.out.println(resultset.getString(1));
-			}
-
-			connection.close();
-		}
-
-		catch (Exception e) {
-			System.out.println(e);
-		}
-	}
+    	protected ResultSet callDB(String startMonth, String endMonth, int startYear, int endYear, String region) {
+    	try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/database","root","Luanamade!");
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM database.location WHERE location = ? AND LENGTH(location) = LENGTH(?) AND ((year = ? AND month >= ?) OR (year > ? AND year < ?) OR (year = ? AND month <= ?)) ORDER BY location, year, month");
+            statement.setString(1, region);
+            statement.setString(2, region);
+            statement.setInt(3, startYear);
+            statement.setString(4, startMonth);
+            statement.setInt(5, startYear);
+            statement.setInt(6, endYear);
+            statement.setInt(7, endYear);
+            statement.setString(8, endMonth);
+            ResultSet resultSet = statement.executeQuery();
+            
+//            connection.close();
+            return resultSet;
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+		return null;
+    }
 }
