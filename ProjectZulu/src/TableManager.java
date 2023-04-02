d0d87070d6c35f0ea89d08460ab46effe346cd31
Wiens-Soshycki-John <77304157+Wiens-Soshycki-John@users.noreply.github.com>
2023-04-02 16:38:02 -0400

    Update TableManager.java

--- ProjectZulu/src/TableManager.java
+++ ProjectZulu/src/TableManager.java
@@ -1,31 +1,60 @@
+package my.custom.area;
+
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class TableManager implements NHPIPublisher {
-	int numOfTables;
-	List<DataTable> l;
+	List<DataTable> listOfTables;
+	List<TableManagerSubscriber> listOfSubs;
+	int expectedTables;
+	Adapter d;
 	
-	protected void clearTables() {
-		l.clear();
+	
+	TableManager(Adapter a){
+		this.d = a;
+		this.listOfTables = new ArrayList<DataTable>();
+		this.listOfSubs = new ArrayList<TableManagerSubscriber>();
 	}
-	
-	public void requestDataTable() {
+
+	@Override
+	public DataTable requestDataTable(String date1, String date2, String location) {
+		DataTable d = this.d.getFilledDataTable(date1, date2, location);
+		listOfTables.add(d);
+		if (listOfTables.size() == expectedTables) {
+			notifySubscribers();
+		}
+		return d;
+	}
+
+	@Override
+	public void addSubscriber(TableManagerSubscriber t) {
+		// TODO Auto-generated method stub
+		this.listOfSubs.add(t);
+	}
+
+	@Override
+	public void unSubscribe(TableManagerSubscriber t) {
+		// TODO Auto-generated method stub
+		this.listOfSubs.remove(t);
 		
 	}
-	
+
+	@Override
 	public void notifySubscribers() {
-		
-	}
-
-	@Override
-	public void unSubscribe() {
 		// TODO Auto-generated method stub
-		
+		Iterator<TableManagerSubscriber> i = listOfSubs.iterator();
+		while (i.hasNext()) {
+			i.next().update(listOfTables);
+		}
 	}
-
+	
 	@Override
-	public void addSubscriber() {
+	public void setExpectedTables(int i) {
 		// TODO Auto-generated method stub
-		
+		this.expectedTables = i;
 	}
-
+	
+	
+	
 }
