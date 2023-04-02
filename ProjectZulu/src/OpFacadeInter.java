4632624e9ec8415eade2ad0f5cecf6cceb3be31b
Wiens-Soshycki-John <77304157+Wiens-Soshycki-John@users.noreply.github.com>
2023-04-02 17:15:15 -0400

    Update OpFacadeInter.java

--- ProjectZulu/src/OpFacadeInter.java
+++ ProjectZulu/src/OpFacadeInter.java
@@ -2,6 +2,7 @@ package my.custom.area;
 
 public interface OpFacadeInter {
 	void addRegion(String location, String date1, String date2);
+	void addTimeSeries(String date1, String date2);
 	void resetDataTables();
 	void setTableManager(TableManager t);
 	String LaunchStatTest();
