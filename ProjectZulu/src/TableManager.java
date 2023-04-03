a40ad406436d1d8e7567907c0da6e5e53e1c04bc
Wiens-Soshycki-John <77304157+Wiens-Soshycki-John@users.noreply.github.com>
2023-04-03 17:16:34 -0400

    Add files via upload

--- ProjectZulu/src/TableManager.java
+++ ProjectZulu/src/TableManager.java
@@ -1,55 +1,100 @@
+package my.custom.area;
+
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class TableManager implements NHPIPublisher {
+public class TableManager implements NHPIPublisher, TableRetriever {
 	List<DataTable> listOfTables;
 	List<TableManagerSubscriber> listOfSubs;
+	List<String> locations;
+	List<String> dates;
 	int expectedTables;
 	Adapter d;
 	
-	TableManager(Adapter a){
-		this.d = a;
+	//constructor
+	TableManager(){
 		this.listOfTables = new ArrayList<DataTable>();
 		this.listOfSubs = new ArrayList<TableManagerSubscriber>();
+		this.dates = new ArrayList<String>();
+		this.locations = new ArrayList<String>();
 	}
-
-	@Override
+	
+	public void setAdapter(Adapter a) {
+		this.d = a;
+	}
+	
+	
 	public DataTable requestDataTable(String date1, String date2, String location) {
 		DataTable d = this.d.getFilledDataTable(date1, date2, location);
 		listOfTables.add(d);
-		
-		if (listOfTables.size() == expectedTables) {
-			notifySubscribers();
-		}
-		
 		return d;
 	}
 
+	
+	
+	//Publisher observer pattern related method
 	@Override
 	public void addSubscriber(TableManagerSubscriber t) {
-		// TODO Auto-generated method stub
 		this.listOfSubs.add(t);
 	}
 
 	@Override
 	public void unSubscribe(TableManagerSubscriber t) {
-		// TODO Auto-generated method stub
 		this.listOfSubs.remove(t);
+		
 	}
 
 	@Override
 	public void notifySubscribers() {
-		// TODO Auto-generated method stub
-		Iterator<TableManagerSubscriber> i = listOfSubs.iterator();
-		
+		Iterator<TableManagerSubscriber> i = this.listOfSubs.iterator();
 		while (i.hasNext()) {
-			i.next().update(listOfTables);
+			i.next().update(this.listOfTables);
 		}
 	}
+	
+
+
 	@Override
 	public void setExpectedTables(int i) {
 		// TODO Auto-generated method stub
 		this.expectedTables = i;
 	}
+
+
+	//Table retriever methods
+	@Override
+	public void addRegion(String location) {
+		this.locations.add(location);
+	}
+
+	@Override
+	public void addTimeSeries(String date1, String date2) {
+		this.dates.add(date1);
+		this.dates.add(date2);
+	}
+
+	@Override
+	public List<DataTable> getTables() {
+		// TODO Auto-generated method stub
+		for (int x=0; x<this.dates.size()/2; x=x+2) {
+			for (int y=0; y<this.locations.size(); y++) {
+				this.listOfTables.add(this.requestDataTable(this.locations.get(y), this.dates.get(x), this.dates.get(x+1)));
+			}
+		}
+		this.notifySubscribers();
+		return this.listOfTables;
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+		this.locations = new ArrayList<String>();
+		this.dates = new ArrayList<String>();
+		this.listOfTables = new ArrayList<DataTable>();
+	}
+	
+	
+	
+	
 }
