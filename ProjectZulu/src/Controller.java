5a6a2b3e6d8f0c219ede4f194d702706ee066b63
Wiens-Soshycki-John <77304157+Wiens-Soshycki-John@users.noreply.github.com>
2023-04-03 22:43:50 -0400

    Add files via upload

--- ProjectZulu/src/Controller.java
+++ ProjectZulu/src/Controller.java
@@ -1,9 +1,11 @@
+package my.custom.area;
+
 import java.util.List;
 
 public interface Controller {
 	void addRegion(String location);
 	void addTimeSeries(String date1, String date2);
-	void reset();
+	void resetData();
 	void setStatTest();
 	void setCompareNHPI();
 	void setMLForecast();
