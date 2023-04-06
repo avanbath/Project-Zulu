67b25d96e1c072109fede72c2e9ce64684c29a5d
Wiens-Soshycki-John <77304157+Wiens-Soshycki-John@users.noreply.github.com>
2023-04-06 17:42:00 -0400

    Add files via upload

--- ProjectZulu/src/BarGraphCreator.java
+++ ProjectZulu/src/BarGraphCreator.java
@@ -1,26 +1,28 @@
 
 
+
 import java.awt.Dimension;
 import java.util.Iterator;
 import java.util.List;
 
-import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.xy.IntervalXYDataset;
-import org.jfree.data.xy.XYDataset;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.chart.axis.CategoryAxis;
+
+import org.jfree.chart.axis.*;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.renderer.category.LayeredBarRenderer;
 
 public class BarGraphCreator implements GraphCreator{
 	public BarGraphCreator(){
     }
 	
 
-	private XYDataset createDataset(List<DataTable> l) {
-		//creating the XY series object
-		XYSeriesCollection dSet = new XYSeriesCollection();
+	private DefaultCategoryDataset createDataset(List<DataTable> l) {
+
+		DefaultCategoryDataset dSet = new DefaultCategoryDataset();
 		//creating iterator for list of DataTables
 		Iterator<DataTable> tableIterator = l.iterator();
 		
@@ -30,20 +32,25 @@ public class BarGraphCreator implements GraphCreator{
 			DataTable dataTable = tableIterator.next();
 			//create an iterator for the tables nhpi value double list
 			Iterator<Double> doubleIterator = dataTable.getTable().iterator();
-			//create a new series which will be added to the collection
-			XYSeries series = new XYSeries(dataTable.getLocation());
+			boolean first = true;
 			double monthCount = 0;
 			int yearCount = 0;
 			while (doubleIterator.hasNext()) {
-				if (monthCount == 13) {
+				String date = "";
+				System.out.println("Curent month before if " + monthCount);
+				if (monthCount == 12 || first) {
 					monthCount = 0;
+					first = false;
+					date = (dataTable.getStartYear() + yearCount)+(monthCount/12) + "";
+					dSet.addValue(doubleIterator.next(), dataTable.getLocation(), date);
 					yearCount++;
 				}
-				double date = (dataTable.getStartYear() + yearCount)+(monthCount/12);
-				series.add(date,doubleIterator.next());
+				System.out.println("Curent month after if " + monthCount);
+
+				doubleIterator.next();
 				monthCount++;
 			}
-			dSet.addSeries(series);
+
 		}
 		
 		return dSet;
@@ -54,13 +61,26 @@ public class BarGraphCreator implements GraphCreator{
 		int difference = t.get(0).getEndYear() - t.get(0).getStartYear();
 		System.out.println("Size is " + difference);
 		String title = "NHPI for " + t.get(0).getStartYear() + "-" + t.get(0).getEndYear();
-		JFreeChart bChart = ChartFactory.createXYBarChart(title, "Years", false, "NHPI", (IntervalXYDataset) createDataset(t));
-        //JFreeChart lChart = ChartFactory.createXYLineChart(title, "Years", "NHPI", createDataset(t), PlotOrientation.VERTICAL, true, true, false);
-       
-		ChartPanel chartPanel = new ChartPanel( bChart );
-       
-        //chartPanel.setPreferredSize(new java.awt.Dimension(10 * (difference), 367));
-        chartPanel.setSize(new Dimension(800, 367));
-        return chartPanel;
+		CategoryAxis cAxis = new CategoryAxis("Year");
+		ValueAxis vAxis = new NumberAxis("Years");
+		CategoryPlot cPlot = new CategoryPlot(createDataset(t), cAxis, vAxis, new LayeredBarRenderer());
+		cPlot.setOrientation(PlotOrientation.VERTICAL);
+		
+		JFreeChart barChart = new JFreeChart(title, JFreeChart.DEFAULT_TITLE_FONT, cPlot, true);
+		
+		LayeredBarRenderer render = (LayeredBarRenderer) cPlot.getRenderer();
+		render.setSeriesBarWidth(0, 1.0);
+        render.setSeriesBarWidth(1, 0.7);
+        render.setSeriesBarWidth(2, 0.5);
+        render.setItemMargin(0.01);
+
+        CategoryAxis domain = cPlot.getDomainAxis();
+        domain.setCategoryMargin(0.25);
+        domain.setUpperMargin(0.05);
+        domain.setLowerMargin(0.05);
+        
+        ChartPanel panel = new ChartPanel(barChart);
+        panel.setSize(new Dimension(800, 367));
+        return panel;
 	}
 }
--- ProjectZulu/src/VisualMaker.java
+++ ProjectZulu/src/VisualMaker.java
@@ -40,56 +40,51 @@ public class VisualMaker implements TableManagerSubscriber, FactoryCommunicator 
 
 	@Override
 	public void update(List<DataTable> l) {
-		System.out.println("Now in the visual maker");
 		// TODO Auto-generated method stub
 		if (this.creator == null) {
 			System.out.println("You must set a creator before getting a visual");
 		}
-		System.out.println("The amount of dataTables is " + l.size());
-		//scrolling window setup
-		JPanel panel=new JPanel();
-        JScrollPane scrollBar=new JScrollPane(panel,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-        JFrame frame = new JFrame();
-        frame.setSize(800,367);
-        //loop setup
-        int counter = 0;
-        int otherCounter = 0;
-        //creating iterator for list of DataTables
-		Iterator<DataTable> i = l.iterator();
-		//creating a list that contains tables of the same time series
-		List<DataTable> sameSeries = new ArrayList<DataTable>();
-		sameSeries.add((DataTable) i.next());
-		System.out.println("First Table added " +  sameSeries.get(0).getLocation());
-		while (i.hasNext()) {
-			DataTable current = (DataTable) i.next();
-			if (sameSeries.get(sameSeries.size()-1).checkSameSeries(current)){
-				sameSeries.add(current);
-				System.out.println("Added a table for " + current.getLocation());
-				otherCounter++;
-				System.out.println(otherCounter);
+		else {
+			System.out.println("The amount of dataTables is " + l.size());
+			//scrolling window setup
+			JPanel panel=new JPanel();
+	        JScrollPane scrollBar=new JScrollPane(panel,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+	        JFrame frame = new JFrame();
+	        frame.setSize(800,367);
+	        //loop setup
+	        int counter = 0;
+	        //creating iterator for list of DataTables
+			Iterator<DataTable> i = l.iterator();
+			//creating a list that contains tables of the same time series
+			List<DataTable> sameSeries = new ArrayList<DataTable>();
+			sameSeries.add((DataTable) i.next());
+			System.out.println("First Table added " +  sameSeries.get(0).getLocation());
+			while (i.hasNext()) {
+				DataTable current = (DataTable) i.next();
+				if (sameSeries.get(sameSeries.size()-1).checkSameSeries(current)){
+					sameSeries.add(current);
+				}
+				else {
+					System.out.println("Requesting chart panel for " + sameSeries.get(0).getStartYear() + ", " + sameSeries.get(0).getEndYear());
+					ChartPanel chart = this.creator.create(sameSeries);
+					System.out.println("Displaying chart panel");
+					panel.add(chart);
+					sameSeries = new ArrayList<DataTable>();
+					sameSeries.add(current);
+					counter++;
+				}
 			}
-			else {
-				System.out.println("Requesting chart panel for " + sameSeries.get(0).getStartYear() + ", " + sameSeries.get(0).getEndYear());
-				ChartPanel chart = this.creator.create(sameSeries);
-				System.out.println("Displaying chart panel");
-				panel.add(chart);
-				sameSeries = new ArrayList<DataTable>();
-				sameSeries.add(current);
-				counter++;
-				otherCounter++;
-				System.out.println(otherCounter);
-			}
+			//System.out.println("Creating another a final chart");
+			System.out.println("Requesting chart panel for " + sameSeries.get(0).getStartYear() + ", " + sameSeries.get(0).getEndYear());
+			ChartPanel chart = this.creator.create(sameSeries);
+			System.out.println("Displaying chart panel");
+			//System.out.println("Finished the final chart");
+			panel.add(chart);
+			counter++;
+			frame.getContentPane().add(scrollBar);
+	        frame.setLayout(new GridLayout(1,counter));
+	        frame.setVisible(true);
 		}
-		//System.out.println("Creating another a final chart");
-		System.out.println("Requesting chart panel for " + sameSeries.get(0).getStartYear() + ", " + sameSeries.get(0).getEndYear());
-		ChartPanel chart = this.creator.create(sameSeries);
-		System.out.println("Displaying chart panel");
-		//System.out.println("Finished the final chart");
-		panel.add(chart);
-		counter++;
-		frame.getContentPane().add(scrollBar);
-        frame.setLayout(new GridLayout(1,counter));
-        frame.setVisible(true);
 		
 	}
 
