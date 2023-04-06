fc5f9e414b22a94cd228f5e9691c4bac8e216bab
Wiens-Soshycki-John <77304157+Wiens-Soshycki-John@users.noreply.github.com>
2023-04-05 23:12:32 -0400

    Add files via upload

--- ProjectZulu/src/LineGraphCreator.java
+++ ProjectZulu/src/LineGraphCreator.java
@@ -1,52 +1,75 @@
+
+
 import java.util.List;
+
 import org.jfree.chart.*;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.category.SlidingCategoryDataset;
+
 import java.util.Iterator;
 import org.jfree.data.xy.*;
+//update to git hub
+
 
 public class LineGraphCreator implements GraphCreator {
+	
+	
 	public LineGraphCreator(){
     }
 	
+
 	private XYDataset createDataset(List<DataTable> l) {
-		// Create the XY series object
+		//creating the XY series object
 		XYSeriesCollection dSet = new XYSeriesCollection();
-		// Create the iterator for the list of DataTables
+		//creating iterator for list of DataTables
 		Iterator<DataTable> tableIterator = l.iterator();
 		
-		// While each individual table has not been added
+		//while each individual table has not been added
 		while (tableIterator.hasNext()) {
-			// Go to the closest unused table
+			//go to closest unused table
 			DataTable dataTable = tableIterator.next();
-			// Create an iterator for the table's NHPI value double list
+			//create an iterator for the tables nhpi value double list
 			Iterator<Double> doubleIterator = dataTable.getTable().iterator();
-			// Create a new series which will be added to the collection
+			//create a new series which will be added to the collection
 			XYSeries series = new XYSeries(dataTable.getLocation());
-			
-			int count = 0;
-			
+			double monthCount = 0;
+			int yearCount = 0;
 			while (doubleIterator.hasNext()) {
-				series.add(dataTable.getStartYear() + count ,doubleIterator.next());
-				count++;
+				if (monthCount == 13) {
+					monthCount = 0;
+					yearCount++;
+				}
+				
+				double date = (dataTable.getStartYear() + yearCount)+(monthCount/12);
+						//dataTable.getStartYear() + count 
+				series.add(date,doubleIterator.next());
+				monthCount++;
+				//System.out.println("Current year added is " + (dataTable.getStartYear() + count));
 			}
-			
 			dSet.addSeries(series);
 		}
 		
 		return dSet;
 	}
 	
+	
 	public ChartPanel create(List<DataTable> t) {
 		String title = "NHPI for " + t.get(0).getStartYear() + "-" + t.get(0).getEndYear();
 		JFreeChart lChart = ChartFactory.createXYLineChart(title, "Years", "NHPI", createDataset(t), PlotOrientation.VERTICAL, true, true, false);
+        //JFreeChart lChart = ChartFactory.createXYLineChart(title, "Years", "NHPI", createDataset(t), PlotOrientation.VERTICAL, true, true, false);
         ChartPanel chartPanel = new ChartPanel( lChart );
         chartPanel.setPreferredSize(new java.awt.Dimension(560, 367));
-        
-        // Return the chartPanel to the visual updater that sends it to the UI
         return chartPanel;
 	}
+
+	//return chartpanel to visual updater that then sends to the ui
+	
+	
+	
+	
+	
+
 }
--- ProjectZulu/src/TableManager.java
+++ ProjectZulu/src/TableManager.java
@@ -1,7 +1,8 @@
+
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Iterator;
-
+//update to github
 public class TableManager implements NHPIPublisher, TableRetriever {
 	List<DataTable> listOfTables;
 	List<TableManagerSubscriber> listOfSubs;
@@ -10,7 +11,7 @@ public class TableManager implements NHPIPublisher, TableRetriever {
 	int expectedTables;
 	Adapter d;
 	
-	// Constructor method
+	//constructor
 	TableManager(){
 		this.listOfTables = new ArrayList<DataTable>();
 		this.listOfSubs = new ArrayList<TableManagerSubscriber>();
@@ -22,11 +23,14 @@ public class TableManager implements NHPIPublisher, TableRetriever {
 		this.d = a;
 	}
 	
+	
 	public DataTable requestDataTable(String date1, String date2, String location) {
 		DataTable d = this.d.getFilledDataTable(date1, date2, location);
 		return d;
 	}
 
+	
+	
 	//Publisher observer pattern related method
 	@Override
 	public void addSubscriber(TableManagerSubscriber t) {
@@ -46,28 +50,41 @@ public class TableManager implements NHPIPublisher, TableRetriever {
 			i.next().update(this.listOfTables);
 		}
 	}
+	
+
+
+
+
 
 	//Table retriever methods
 	@Override
 	public void addRegion(String location) {
 		this.locations.add(location);
 	}
-	
+
 	@Override
 	public void addTimeSeries(String date1, String date2) {
 		this.dates.add(date1);
 		this.dates.add(date2);
 	}
-	
+
 	@Override
 	public List<DataTable> getTables() {
 		// TODO Auto-generated method stub
-		for (int x=0; x<this.dates.size()/2; x += 2) {
+		//for loop should handle getting time series and add seperately
+		System.out.println("The number of time series is " + (this.dates.size()/2) + " raw value is " + this.dates.size());
+		System.out.println("The number of locations is " + this.locations.size());
+		for (int x=0; x<this.dates.size(); x=x+2) {
+			System.out.println("The current value of x is " + x);
 			for (int y=0; y<this.locations.size(); y++) {
+				//System.out.println("Trying to get a table for " + this.locations.get(y) + " " + this.dates.get(x) + " " + this.dates.get(x+1));
+				System.out.println("The current value of y is " + y);
 				this.listOfTables.add(this.requestDataTable(this.dates.get(x), this.dates.get(x+1), this.locations.get(y)));
-				System.out.println("Trying to get a table for region: " + this.locations.get(y) + ", start date: " + this.dates.get(x) + ", end date: " + this.dates.get(x+1));
+				System.out.println("The amount of dataTables made is! " + this.listOfTables.size());
+				//System.out.println("Trying to get a table for " + this.locations.get(y) + " " + this.dates.get(x) + " " + this.dates.get(x+1));
 			}
 		}
+		System.out.println("The amount of dataTables made is* " + this.listOfTables.size());
 		this.notifySubscribers();
 		return this.listOfTables;
 	}
@@ -79,4 +96,10 @@ public class TableManager implements NHPIPublisher, TableRetriever {
 		this.dates = new ArrayList<String>();
 		this.listOfTables = new ArrayList<DataTable>();
 	}
+	
+	
+	
+	
 }
+
+
